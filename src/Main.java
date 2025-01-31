2aeca0f6da8ac6cc38f16dd3214b325e7a7b9c96
Dave-93 <difrancesco.davide93@gmail.com>
2025-02-01 00:18:44 +0100

    feat: exercise

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,24 @@ public class Main {
     
     public static void main(String[] args) {
         
+        Prodotto laptop = new Prodotto("lenovo", 500, 22);//INIZIALIZZAZIONE DINAMICA DELL'ISTANZA TRAMITE PARAMETRI
+        //INIZIALIZZAZIONE MANUALE
+        /* laptop.prezzo = 500;
+        laptop.iva = 22;
+        laptop.nome = "LENOVO"; */
+
+        Prodotto smartphone = new Prodotto("Samsung", 899, 22);
+
+        //RICHIAMO DEI METODI DALL'ISTANZA
+        laptop.codiceRandom();
+        laptop.prezzoBase();
+        laptop.prezzoIvato();
+        laptop.nomeEsteso(); 
+
+        smartphone.codiceRandom();
+        smartphone.prezzoBase();
+        smartphone.prezzoIvato();
+        smartphone.nomeEsteso();
+        // 
     }
 }
--- src/Prodotto.java
+++ src/Prodotto.java
@@ -1,3 +1,41 @@
 public class Prodotto {
    
+    //ATTRIBUTI
+    public int codice;
+    public String nome;
+    public String descrizione;
+    public int prezzo;
+    public int iva;
+    //
+
+    //COSTRUTTORE con PARAMETRI se dentro le parentesi aggiungiamo dei valori richiesti
+    public Prodotto(String nome, int prezzo, int iva){
+        this.nome = nome;
+        this.prezzo = prezzo;
+        this.iva = iva;
+    }
+    //
+
+    //METODI
+    public int codiceRandom (){
+        this.codice += (int) ((Math.random() * 100) + 1);//così genera un numero tra 1 e 100
+        //System.out.println("Il codice generato è: " + codice); per verificare la creazione del codice
+        return codice;
+    } 
+
+    public void prezzoBase(){
+        this.prezzo = prezzo;
+        System.out.println("Il prezzo base del prodotto " + nome + " è: " + prezzo);
+    }
+
+    public void prezzoIvato(){
+        int prezzoIvato = prezzo + (prezzo * iva)/100 ;
+        System.out.println("Il prezzo ivato del prodotto " + nome + " è: " + prezzoIvato);
+    }
+
+    public void nomeEsteso(){
+        String codiceConvertitaInString = Integer.toString(codice);//codice + ""; aggiungendo la stringa vuota trasformo l'int in string
+        System.out.println("Il nome esteso del prodotto è: " + codiceConvertitaInString + "-" + nome);
+    }
+    //
 }
